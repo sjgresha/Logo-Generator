72b355892f1ac5d19a1f0cee6b21cccdbf25de3b
Steven Gresham <sbjg2545@gmail.com>
2023-09-19 21:56:09 -0700

    worked on index.js file and created first logo.svg file

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
+//importing packages and classes
 const inquirer = require('inquirer');
 const fs = require('fs');
 const {Circle, Square, Triangle} = require('./lib/shape');
+
+function writeFile(fileName, answers) {
+    let svg = "";
+    svg = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg>`;
+    svg += `${answers.shape}`;
+
+    let shapeChoice;
+    if (answers.shape === "Circle") {
+        shapeChoice = new Circle();
+        svg += `circle cx="115" cy="115" r="80" fill="${answers.shapeColor}"`
+    }
+    //need to add Square and Triangle
+
+    fs.writeFile(fileName, svg, (error) => {
+        error ? console.log(error) : console.log("logo.svg created successfully!");
+    });
+}
 
 function userQuestions() {
     inquirer
@@ -33,7 +51,7 @@ function userQuestions() {
             console.log("Must enter a value of no more than 3 characters");
             userQuestions();
         } else {
-            writeToFile("logo.svg", answers);
+            writeFile("logo.svg", answers);
         }
     });
 }
